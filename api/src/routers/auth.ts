b033b7f80b57ab3b23d89dbc8cef1cb1791f5910
yun8boo <yuma051521@gmail.com>
2021-08-22 23:58:56 +0900

    commit

--- api/src/routers/auth.ts
+++ api/src/routers/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { UserModel } from '../models/User'
 import crypto from 'crypto-js'
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
@@ -24,14 +25,24 @@ router.post('/login', async(req, res) => {
   try {
     const user = await UserModel.findOne({email: req.body.email})
     if(!user) {
-      res.status(401).json('Wrong username')
+      res.status(401).json('Wrong email')
     }
     const bytes = crypto.AES.decrypt(user.password, process.env.SECRET_KEY as string)
-    const originalText = bytes.toString()
+    const originalText = bytes.toString(crypto.enc.Utf8)
+    
     if(originalText !== req.body.password) {
       res.status(401).json('Wrong password')
     }
-    res.status(200).json(user)
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY as string,
+      { expiresIn: '5d'}
+    )
+
+    const {password, ...info} = user._doc
+
+    res.status(200).json({...info, accessToken})
   } catch (err) {
     res.status(500).json(err)
   }
