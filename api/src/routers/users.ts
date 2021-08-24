f36c1fd3cac1b76b41ac75bef37bc4d8d26d6557
yun8boo <yuma051521@gmail.com>
2021-08-24 09:29:39 +0900

    commit

--- api/src/routers/users.ts
+++ api/src/routers/users.ts
@@ -27,8 +27,48 @@ router.put('/:id', verifyToken, async(req: Request, res) => {
 })
 
 // delete
+router.delete('/:id', verifyToken, async(req: Request, res) => {
+  if(req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await UserModel.findByIdAndDelete(req.params.id)
+      res.status(200).json('User has been deleted...')
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }else {
+    res.status(403).json('You can delete only your account!')
+  }
+})
+
 // get
+router.get('/:id', verifyToken, async(req: Request, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id)
+    const { password, ...info } = user._doc
+    res.status(200).json(info)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 // get all
+router.get('/', verifyToken, async(req: Request, res) => {
+  if(req.user.isAdmin) {
+    try {
+      const users = await UserModel.find()
+      const userInfos = users.map((user: any) => {
+        const { password, ...info} = user._doc
+        return info
+      })
+  
+      res.status(200).json(userInfos)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }else {
+    res.status(403).json('You can delete only your account!')
+  }
+})
 // get user stats
 
 export default router
